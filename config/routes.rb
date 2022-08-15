5821429ba9c09be72b5a4fb82b9c75a6510d5189
Andres Munoz <andremude@hotmail.com>
2022-08-14 20:51:20 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  resources :storages do
+    resources :reservations, only: %i[create new]
+  end
 end
