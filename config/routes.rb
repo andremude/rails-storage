5f350dd50e20b99e823c9709f8d691b68b3a957c
Andres Munoz <andremude@hotmail.com>
2022-08-18 18:15:16 -0500

    links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  # , path_names: {sign_in: 'login', sign_out: 'logout'}
   # root to: 'pages#home'
   resources :storages do
     resources :reservations, only: %i[create new]
