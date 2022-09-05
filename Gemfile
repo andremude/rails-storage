1788979962d328dead52822d3787f85f36f28b8b
Andres Munoz <andremude@hotmail.com>
2022-09-05 10:33:14 -0500

    storages index

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootsnap', '>= 1.4.4', require: false
 gem 'devise'
 gem "font-awesome-rails"
 gem 'simple_form'
+gem "active_model_serializers"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,11 +8,30 @@
 
 
 users = [{ email: "johng@example.com", first_name: "John", last_name: "Gibson", document_number: "70025447", password: "123456" },
-         { email: "paulj@example.com", first_name: "Paul", last_name: "Johnson", document_number: "7004789", password: "123456" }
+         { email: "paulj@example.com", first_name: "Paul", last_name: "Johnson", document_number: "7004789", password: "123456" },
+         { email: "johnj@example.com", first_name: "John", last_name: "Jacob", document_number: "7004789", password: "123456" },
+         { email: "carlm@example.com", first_name: "Carl", last_name: "Morrison", document_number: "7004789", password: "123456" },
+         { email: "mariab@example.com", first_name: "Maria", last_name: "Best", document_number: "7004789", password: "123456" },
+         { email: "claudiah@example.com", first_name: "Claudia", last_name: "Harrison", document_number: "7004789", password: "123456" },
+         { email: "angusy@example.com", first_name: "Angus", last_name: "Young", document_number: "7004789", password: "123456" },
+         { email: "mickj@example.com", first_name: "Mick", last_name: "Jagger", document_number: "7004789", password: "123456" },
+         { email: "pauld@example.com", first_name: "Paul", last_name: "Davis", document_number: "7004789", password: "123456" }
         ]
 
-storages = [{ title: "accessible storage in city center", meters: '20', address: "175 Midway Ave", city: "London", price: 20.00, user_id: 2 },
-         { title: "Spaceous storage", meters: '45', address: "84 Coventry Ave", city: "London", price: 50.00, user_id: 1 }
+storages = [{ title: "accessible storage in city center", meters: '20', address: "175 Midway Ave", city: "London", price: 20.00, user_id: 1 },
+            { title: "Spaceous storage", meters: '45', address: "84 Coventry Ave", city: "Brussels", price: 50.00, user_id: 2 },
+            { title: "Lorem Ipsum", meters: '45', address: "Lorem Ipsum", city: "Amsterdam", price: 50.00, user_id: 3 },
+            { title: "Lorem Ipsum", meters: '55', address: "Lorem Ipsum", city: "Madrid", price: 50.00, user_id: 4 },
+            { title: "Lorem Ipsum", meters: '65', address: "Lorem Ipsum", city: "Barcelona", price: 50.00, user_id: 5 },
+            { title: "Lorem Ipsum", meters: '15', address: "Lorem Ipsum", city: "Los Angeles", price: 50.00, user_id: 6 },
+            { title: "Lorem Ipsum", meters: '25', address: "Lorem Ipsum", city: "Forida", price: 50.00, user_id: 7 },
+            { title: "Lorem Ipsum", meters: '35', address: "Lorem Ipsum", city: "London", price: 50.00, user_id: 8 },
+            { title: "Lorem Ipsum", meters: '45', address: "Lorem Ipsum", city: "Lisbon", price: 50.00, user_id: 9 },
+            { title: "Lorem Ipsum", meters: '55', address: "Lorem Ipsum", city: "London", price: 50.00, user_id: 1 },
+            { title: "Lorem Ipsum", meters: '15', address: "Lorem Ipsum", city: "Brussels", price: 50.00, user_id: 2 },
+            { title: "Lorem Ipsum", meters: '25', address: "Lorem Ipsum", city: "San Francisco", price: 50.00, user_id: 3 },
+            { title: "Lorem Ipsum", meters: '35', address: "Lorem Ipsum", city: "Paris", price: 50.00, user_id: 4 },
+            { title: "Lorem Ipsum", meters: '45', address: "Lorem Ipsum", city: "Amsterdam", price: 50.00, user_id: 5 },
         ]
 
 
