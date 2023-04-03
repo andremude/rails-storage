b465c060e499ac4625ffcfcb01b29f9cda98a564
Andres Munoz <andremude@hotmail.com>
2023-04-03 12:29:31 -0500

    fix new registration devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}
+  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout'}, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: 'pages#home'
 
   resources :storages do
